a965726a3cb6cd42651f05dcbd31b6d9559bcb74
stefanfjellsten <63704434+stefanfjellsten@users.noreply.github.com>
2026-01-06 18:04:28 +0100

    larger cities

--- src/entities/City.ts
+++ src/entities/City.ts
@@ -13,7 +13,7 @@ export class City implements Entity {
         const building = model.clone()
 
         // Scale down
-        building.scale.set(0.1, 0.1, 0.1)
+        building.scale.set(0.15, 0.15, 0.15)
 
         // Orient Upright (FBX often Z-up vs Game Y-up, but Three loader tries to fix it)
         // If user says "more upright", maybe it's tilted?
